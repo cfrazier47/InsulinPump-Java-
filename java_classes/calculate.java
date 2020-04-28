877f217c8766d7a3385aee8520351929d29d4dc5
Courtney Frazier <48774616+cfrazier47@users.noreply.github.com>
2020-04-27 23:51:48 -0400

    Calculates Insulin Dosage
    Currently all this does is print "Testing!" to the console whenever the button "Get Insulin" (from insulin.xml) is pressed.

---
+++ java_classes/calculate.java
@@ -0,0 +1,18 @@
+package com.example.insulinpump;
+import com.example.insulinpump.needle;
+import com.example.insulinpump.sensor;
+
+public class calculate {
+    // Global variables
+    int insulin = 0;
+
+    // insulin calculations will take place here
+    public static void calculate(android.view.View View) {
+        System.out.println("Testing!"); //**********************************************************TESTING
+    }
+
+    // output goes to needle
+    public static void administerInsulin() {
+        // refer to sensor.someFunction()
+    }
+}
