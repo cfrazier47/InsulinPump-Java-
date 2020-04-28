115052ee941f2bacb65f6467256afc14941e2102
Courtney Frazier <48774616+cfrazier47@users.noreply.github.com>
2020-04-27 23:49:42 -0400

    (Currently Unnecessary)
    Log setup and update commands will eventually go here.

---
+++ java_classes/log.java
@@ -0,0 +1,8 @@
+package com.example.insulinpump;
+import com.example.insulinpump.database_configuration;
+
+public class log {
+    // setting up log database goes here (if not already created)
+
+    // log commands go here
+}
